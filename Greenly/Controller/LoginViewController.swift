5659ad639504844d31e3fb7282d9a9c1472904e1
sara <saraabdulrahim19@gmail.com>
2024-12-03 15:28:16 +0300

    edited the codes for the show password/hide password and the login button to all the interfaces

--- Greenly/Controller/LoginViewController.swift
+++ Greenly/Controller/LoginViewController.swift
@@ -12,11 +12,14 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var showPasswordButton: UIButton! // Button to toggle password visibility
 
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+            super.viewDidLoad()
+            // Set initial state for password field
+            passwordTextField.isSecureTextEntry = true
+        }
 
     @IBAction func loginButtonTapped(_ sender: UIButton) {
            guard let email = emailTextField.text, let password = passwordTextField.text else { return }
@@ -29,8 +32,10 @@ class LoginViewController: UIViewController {
                        performSegue(withIdentifier: "showAdminHomePage", sender: self)
                    case "store owner":
                        performSegue(withIdentifier: "showStoreOwnerHomePage", sender: self)
-                   default:
+                   case "user":
                        performSegue(withIdentifier: "showUserHomePage", sender: self)
+                   default:
+                       break
                    }
                    return
                }
@@ -41,6 +46,15 @@ class LoginViewController: UIViewController {
         let alert = UIAlertController(title: "Error", message: "Invalid email or password.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
+    }
+    
+    @IBAction func showPasswordTapped(_ sender: UIButton) {
+        // Toggle secure text entry
+        passwordTextField.isSecureTextEntry.toggle()
+        
+        // Change button appearance based on password visibility
+        let buttonImage = passwordTextField.isSecureTextEntry ? "eye.slash" : "eye"
+        showPasswordButton.setImage(UIImage(systemName: buttonImage), for: .normal)
     }
 
     @IBAction func createAccountTapped(_ sender: UIButton) {
