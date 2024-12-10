b1293b08d720cd5596454aa222352dab61a8af5b
sara <saraabdulrahim19@gmail.com>
2024-12-10 15:47:04 +0300

    added suggested products view

---
+++ Greenly/Controller/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  Greenly
+//
+//  Created by BP-36-201-20 on 10/12/2024.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+}
