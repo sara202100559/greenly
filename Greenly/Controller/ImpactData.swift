e2d386cc15140264ebac407442dbf5416af7bb20
Q <202001256@student.polytechnic.bh>
2024-12-30 13:03:10 +0300

    impact.tracker

--- Greenly/Controller/ImpactData.swift
+++ Greenly/Controller/ImpactData.swift
@@ -1,18 +1,31 @@
-////
-////  ImpactData.swift
-////  tracker
-////
-////  Created by BP-36-201-22 on 20/12/2024.
-////
 //
-//import Foundation
+//  ImpactData.swift
+//  tracker
 //
-//class ImpactData {
-//    // Singleton instance
-//    static let shared = ImpactData()
+//  Created by BP-36-201-22 on 20/12/2024.
 //
-//    // Dictionary to store chart data
-//    var chartData: [String: Double] = [:] // Store impact values for categories
-//
-//    private init() {} // Private initializer to prevent instantiation
-//}
+
+import Foundation
+
+class ImpactData {
+    // Singleton instance
+    static let shared = ImpactData()
+
+    // Dictionary to store chart data
+    var chartData: [String: Double] = [:] // Store impact values for categories
+
+    private init() {} // Private initializer to prevent instantiation
+    
+
+
+      
+        // Method to reset the impact data to default values
+        func resetToDefaultValues() {
+            chartData = [
+                "Plastic Waste Reduction Product": 2.0,
+                "Water Conservation Product": 3.0,
+                "Carbon Emission Reduction": 2.0,
+                "Others": 0.5
+            ]
+        }
+    }
